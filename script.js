22571ecf41ecf46803724f0dbf354183216d81b1
ola <henzoca.2020@gmail.com>
2024-12-09 17:44:39 -0300

    V-1.002

--- script.js
+++ script.js
@@ -1,44 +1,85 @@
+let activateScreen = {};
+
+const globals = {};
+
 const sprites = new Image();
 sprites.src = "./sprites.png";
+
+const soundHit = new Audio();
+soundHit.src = "./sounds/hit.wav";
+
+const soundPulo = new Audio();
+soundPulo.src = "./sounds/pulo.wav";
 
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
 
-const player = {
-	spriteY: 0,
-	spriteX: 0,
-	width: 33,
-	height: 24,
-	x: 10,
-	y: canvas.height /2 -12,
-	weight: 0.5,
-	aceleration: 0,
-	gravity() {
-		this.aceleration += this.weight;
-		this.y += this.aceleration;
-	},
-	draw() {
-		context.drawImage(
-			sprites,
-			this.spriteX,
-			this.spriteY,
-			this.width,
-			this.height,
-			this.x,
-			this.y,
-			this.width,
-			this.height
-		);
-	},
-};
+function colliding(obj1, obj2) {
+	if (obj1.y + obj1.height >= obj2.y || obj1.y <= 0) {
+		return true;
+	} else {
+		return false;
+	}
+}
+function setPlayer() {
+	const player = {
+		spriteY: 0,
+		spriteX: 0,
+		width: 33,
+		height: 24,
+		x: 10,
+		jump: 4.6,
+		up() {
+			soundPulo.play();
+			this.aceleration = -this.jump;
+		},
+		y: canvas.height / 2 - 24,
+		weight: 0.25,
+		aceleration: 0,
+		gravity() {
+			if (colliding(player, globals.ground)) {
+				soundHit.play();
+				setTimeout(() => {
+					changeScreen(screens.init);
+				}, 500);
+
+				return;
+			}
+
+			this.aceleration += this.weight;
+			this.y += this.aceleration;
+		},
+		spritesMoving: [
+			{spriteX:0 , spriteY:0},
+			{spriteX:0 , spriteY:26},
+			{spriteX:0 , spriteY:52}
+		],
+		draw() {
+			const {spriteX,spriteY} = this.spritesMoving[0];
+
+			context.drawImage(
+				sprites,
+				this.spriteX,
+				this.spriteY,
+				this.width,
+				this.height,
+				this.x,
+				this.y,
+				this.width,
+				this.height
+			);
+		},
+	};
+	return player;
+}
 
 const introduction = {
 	spriteY: 0,
 	spriteX: 134,
 	width: 174,
 	height: 152,
-	x: (canvas.width/2) - 87,
-	y: canvas.height/2 - 76,
+	x: canvas.width / 2 - 87,
+	y: canvas.height / 2 - 76,
 	draw() {
 		context.drawImage(
 			sprites,
@@ -53,46 +94,57 @@ const introduction = {
 		);
 	},
 };
+function setGround() {
+	const ground = {
+		spriteY: 610,
+		spriteX: 0,
+		width: 224,
+		height: 112,
+		x: 0,
+		y: canvas.height - 112,
+		actualize() {
+			const velocityMoviment = 1;
+			const repete = this.width / 2;
+			this.x -= velocityMoviment;
 
-const ground = {
-	spriteY: 610,
-	spriteX: 0,
-	width: 224,
-	height: 112,
-	x: 0,
-	y: canvas.height - 112,
-	draw() {
-		context.drawImage(
-			sprites,
-			this.spriteX,
-			this.spriteY,
-			this.width,
-			this.height,
-			this.x,
-			this.y,
-			this.width,
-			this.height
-		);
-		context.drawImage(
-			sprites,
-			this.spriteX,
-			this.spriteY,
-			this.width,
-			this.height,
-			this.x + this.width,
-			this.y,
-			this.width,
-			this.height
-		);
-	},
-};
+			if (this.x == -repete) {
+				this.x = 0;
+			}
+		},
+		draw() {
+			context.drawImage(
+				sprites,
+				this.spriteX,
+				this.spriteY,
+				this.width,
+				this.height,
+				this.x,
+				this.y,
+				this.width,
+				this.height
+			);
+			context.drawImage(
+				sprites,
+				this.spriteX,
+				this.spriteY,
+				this.width,
+				this.height,
+				this.x + this.width,
+				this.y,
+				this.width,
+				this.height
+			);
+		},
+	};
+	return ground;
+}
 const background = {
 	spriteY: 0,
 	spriteX: 390,
 	width: 276,
 	height: 204,
 	x: 0,
-	y: ground.y - ground.height,
+	y: 252,
 	draw() {
 		context.fillStyle = "#70c5ce";
 		context.fillRect(0, 0, canvas.width, canvas.height);
@@ -122,40 +174,51 @@ const background = {
 	},
 };
 
-let activateScreen = {};
-
-function changeScreen(newScreen){
+function changeScreen(newScreen) {
 	activateScreen = newScreen;
+
+	if (activateScreen.initializy) {
+		activateScreen.initializy();
+	}
 }
 
 const screens = {
-	init:{
-		draw(){
+	init: {
+		initializy() {
+			globals.player = setPlayer();
+			globals.ground = setGround();
+		},
+		draw() {
 			background.draw();
-			ground.draw();
-			player.draw();
-			introduction.draw();	
+			globals.ground.draw();
+			globals.player.draw();
+			introduction.draw();
 		},
-		click(){
+		click() {
 			changeScreen(screens.game);
+			globals.player.gravity();
+			globals.player.up();
 		},
-		actualize(){
-
-		}
-	}
-}
+		actualize() {
+			globals.ground.actualize();
+		},
+	},
+};
 
 screens.game = {
-	draw(){
+	draw() {
 		background.draw();
-		ground.draw();
-		player.draw();
+		globals.ground.draw();
+		globals.player.draw();
 	},
-	actualize(){
-		player.gravity();
-	}
-}
-
+	click() {
+		globals.player.up();
+	},
+	actualize() {
+		globals.player.gravity();
+		globals.ground.actualize();
+	},
+};
 
 function drawObjects() {
 	activateScreen.draw();
@@ -164,11 +227,11 @@ function drawObjects() {
 	requestAnimationFrame(drawObjects);
 }
 
-window.addEventListener("click",()=>{
-	if(activateScreen.click){
-		activateScreen.click();	
+canvas.addEventListener("click", () => {
+	if (activateScreen.click) {
+		activateScreen.click();
 	}
-})
+});
 
 changeScreen(screens.init);
 drawObjects();
